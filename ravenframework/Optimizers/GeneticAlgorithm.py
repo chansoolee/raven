1975a0d1612e9e36dd114c9ec8e64eb20892225d
chansoolee <lchs5777@gmail.com>
2026-02-15 16:12:50 +0900

    Add Deduplicate logic

--- ravenframework/Optimizers/GeneticAlgorithm.py
+++ ravenframework/Optimizers/GeneticAlgorithm.py
@@ -360,7 +360,9 @@ class GeneticAlgorithm(RavenSampled):
     self._repairInstance = None                                  # instance of repair
     self._canHandleMultiObjective = True                         # boolean indicator whether optimization is a sinlge-objective problem or a multi-objective problem
     self._sampledPopulationInfo = {}                             # stores population and fitness info
-
+    # Deduplication: skip re-evaluation of previously evaluated individuals
+    self._deduplication = False                                  # on/off toggle for skipping duplicate evaluations
+    self._evaluatedIndividuals = set()                           # set of hashable keys (tuples) for all evaluated chromosomes across generations
   ##########################
   # Initialization Methods #
   ##########################
@@ -573,6 +575,20 @@ class GeneticAlgorithm(RavenSampled):
         descr=r""" normalize: input and output data will be normalized prior to calculating fitness for each iteration of the optimizer. \default{zscore}""")
     fitness.addSub(normalizeFitness)
     GAparams.addSub(fitness)
+
+    # Deduplication
+    deduplication = InputData.parameterInputFactory('deduplication', strictMode=True,
+        contentType=InputTypes.BoolType,
+        printPriority=108,
+        descr=r"""If True, enables deduplication of the population across generations.
+                  When a new generation of children is created, any individual whose
+                  gene combination has already been evaluated in a previous generation
+                  will be skipped (not submitted for simulation), saving computational cost.
+                  The optimizer tracks all uniquely evaluated individuals using a
+                  memory-efficient set of hash keys.
+                  \default{False}""")
+    GAparams.addSub(deduplication)
+
     specs.addSub(GAparams)
 
     # convergence
@@ -740,6 +756,13 @@ class GeneticAlgorithm(RavenSampled):
     self._repairInstance = repairReturnInstance(self,name='replacementRepair')  # currently only replacement repair is implemented.
 
     ####################################################################################
+    # deduplication node                                                               #
+    ####################################################################################
+    deduplicationNode = gaParamsNode.findFirst('deduplication')
+    if deduplicationNode is not None:
+      self._deduplication = deduplicationNode.value
+
+    ####################################################################################
     # convergence criterion node                                                       #
     ####################################################################################
     convNode = paramInput.findFirst('convergence')
@@ -818,6 +841,10 @@ class GeneticAlgorithm(RavenSampled):
 
 
     currentPopInputs = datasetToDataArray(rlz, list(self.toBeSampled))
+
+    # Cache evaluated individuals for deduplication (track all chromosomes seen so far)
+    if self._deduplication:
+      self._cacheEvaluatedPopulation(currentPopInputs)
 
     currentPop_objvals = []
     for i in range(len(self._objectiveVar)):
@@ -983,15 +1010,75 @@ class GeneticAlgorithm(RavenSampled):
                                         'Gene':list(self.toBeSampled)})
 
   ## 10. Submit children batch
+      # First, build a list of all children as dicts
+      childrenToSubmit = []
+
       # Submit children coordinates (x1,...,xm), i.e., self.childrenCoordinates
       for i in range(self.batch):
         newRlz = {}
         for _, var in enumerate(self.toBeSampled.keys()):
           newRlz[var] = float(daChildren.loc[i, var].values)
-        self._submitRun(newRlz, traj, self.getIteration(traj))
+
+        childrenToSubmit.append(newRlz)
+
+      # Deduplication: filter out children already evaluated in previous generations
+      if self._deduplication:
+        uniqueChildren = []
+        duplicateChildren = []
+        for child in childrenToSubmit:
+          cacheKey = self._makePopulationKey(child)
+          if cacheKey in self._evaluatedIndividuals:
+            duplicateChildren.append(child)
+          else:
+            uniqueChildren.append(child)
+        # Ensure minimum batch of 2 to maintain batch-mode processing
+        # (GA requires xr.Dataset which needs batch > 1)
+        toSubmit = list(uniqueChildren)
+        while len(toSubmit) < 2 and duplicateChildren:
+          toSubmit.append(duplicateChildren.pop(0))
+        skippedCount = len(childrenToSubmit) - len(toSubmit)
+        if skippedCount > 0:
+          self.raiseADebug(f'Deduplication: skipped {skippedCount}/{self._populationSize} '
+                           f'duplicate children (submitting {len(toSubmit)})')
+      else:
+        toSubmit = childrenToSubmit
+
+      self.batch = len(toSubmit)
+      for child in toSubmit:
+        self._submitRun(child, traj, self.getIteration(traj))
 
   ## 11. Save grandparents
     self.prevPop_inputs = deepcopy(currentPopInputs)
+
+  def _makePopulationKey(self, point):
+    """
+      Creates a hashable key from a population member's variable values
+      for use in duplicate detection.
+      Uses sorted variable names for consistent ordering, with values
+      rounded to 10 decimal places to handle floating-point noise.
+      @ In, point, dict, {varName: value} for a single population member
+      @ Out, key, tuple, hashable representation of the point
+    """
+    return tuple(round(float(point[var]), 10) for var in sorted(self.toBeSampled.keys()))
+
+  def _cacheEvaluatedPopulation(self, population):
+    """
+      Adds all individuals from the population DataArray to the evaluated cache.
+      Called at the start of each generation to register newly evaluated chromosomes.
+      @ In, population, xr.DataArray, population with dims ['chromosome', 'Gene']
+      @ Out, None
+    """
+    newCount = 0
+    for i in range(population.shape[0]):
+      point = {str(var): float(population.loc[i, var].values)
+               for var in population.coords['Gene'].values}
+      cacheKey = self._makePopulationKey(point)
+      if cacheKey not in self._evaluatedIndividuals:
+        self._evaluatedIndividuals.add(cacheKey)
+        newCount += 1
+    if newCount > 0:
+      self.raiseADebug(f'Deduplication: cached {newCount} new individual(s). '
+                       f'Total unique evaluations: {len(self._evaluatedIndividuals)}')
 
   def _submitRun(self, point, traj, step, moreInfo=None):
     """
@@ -1031,6 +1118,7 @@ class GeneticAlgorithm(RavenSampled):
     self.hdsm = np.NaN
     self.bestPoint = None
     self.bestFitness = None
+    self._evaluatedIndividuals = set()
     self.multiBestPoint = None
     self.multiBestFitness = None
     self.multiBestObjective = None
